a1f27e1349713756775799cfbce0e20719272fee
MBDesu <jordyn.tucker@protonmail.com>
2024-04-13 16:31:09 -0400

    update default to light theme

--- src/app/common/services/theming.service.ts
+++ src/app/common/services/theming.service.ts
@@ -8,13 +8,21 @@ export class ThemingService {
   isDarkTheme = true;
   themeChanged = output<boolean>();
 
+  constructor() {
+    this.toggleTheme();
+  }
+
   toggleTheme(): void {
     if (this.isDarkTheme) {
       document.body.classList.add('light-theme');
     } else {
       document.body.classList.remove('light-theme');
     }
-    this.isDarkTheme = !this.isDarkTheme;
+    this.onChange(!this.isDarkTheme);
+  }
+
+  onChange(isDarkTheme: boolean): void {
+    this.isDarkTheme = isDarkTheme;
     this.themeChanged.emit(this.isDarkTheme);
   }
 
